494e88fe0ee48eba1aa70fc0412648efa61ea83c
Liang Niu <niuliang3@gmail.com>
2018-03-07 19:04:19 -0500

    add iit-aff related files

---
+++ iitaff.py
@@ -0,0 +1,207 @@
+# %matplotlib inline
+
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+# import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+from config import Config
+import utils
+import model as modellib
+import visualize
+from model import log
+from PIL import Image
+
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+## -------------------------- Configurations ----------------------
+class IITAFFConfig(Config):
+    """Configuration for training on the IIT-AFF dataset.
+    Derives from the base Config class and overrides values specific
+    to the dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "IITAFF"
+
+    # Train on 1 GPU and 2 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 2 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 9  # background + 9 affordances (or 10 object classes)
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    # Please refer to 009: /home/niu/Liang_Niu/IIT_Affordances_2017/rgb(h).txt
+    IMAGE_MIN_DIM = 34
+    IMAGE_MAX_DIM = 2698
+
+    # Use smaller anchors because our image and objects are small
+    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    # TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    # STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    # VALIDATION_STEPS = 5
+    
+config = IITAFFConfig()
+config.display()
+
+## -------------------------- Notebook Preferences ----------------------
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+class IITAFFDataset(utils.Dataset):
+    """ Load IIT AFF Dataset.
+    """
+    def __init__(self):
+        super(IITAFFDataset, self).__init__()
+        self.name = 'IITAFF'
+
+    def load_iitaff( self, dataset_dir = '/home/niu/Liang_Niu/IIT_Affordances_2017', subset = 'train'):
+        """ Load a subset of IIT AFF Dataset
+        dataset_dir: The root directory of the IIT-AFF dataset.
+        subset: What to load (train, val, train_val, test)
+        return_coco: If True, returns the COCO object.
+        auto_download: Automatically download and unzip MS-COCO images and annotations
+        """
+
+        assert subset in ('train', 'val', 'test')
+
+        self.dataset_dir = dataset_dir
+        self.image_dir = os.path.join(dataset_dir, 'rgb')
+        self.aff_dir = os.path.join(dataset_dir, 'affordances_labels')
+        class_names = ['background', 'contain', 'cut', 'display', 'engine',
+                       'grasp', 'hit', 'pound', 'support', 'w-grasp']
+
+        # Add classes
+        for id, name in enumerate(class_names):
+            self.add_class(self.name, id, name)
+
+        # Load all files into list
+        def get_file_list():
+            """ Get name list for train/val/test data.
+            Statistics: 4417 train, 1767 val, 2651 test. In total: 8835.
+            TODO: Later we may want to divide the train_val in a defferent way.
+            Like only take first 500 in val list as val, others will be added into train list.
+            """
+            train_val = []
+            train, val, test = []
+            with open(dataset_dir+'/train_and_val.txt') as f:
+                train_val = [ line.rstrip('\n') for line in f]
+            with open(dataset_dir+'/val.txt') as f:
+                val       = [ line.rstrip('\n') for line in f]
+            with open(dataset_dir+'/test.txt') as f:
+                test      = [ line.rstrip('\n') for line in f]
+            train = list(set(train_val) - set(val))
+            # if shuffle:
+            # random.seed(42)
+            # random.shuffle(train)
+            # random.shuffle(val)
+            # random.shuffle(test)
+            return train, val, test
+        
+        train_names, val_names, test_names = get_file_list()
+        self.id2name = train_names+val_names+test_names
+        self.id_base = {'train':0, 'val':len(train_names), 'test':len(train_names)+len(val_names)}[subset]
+        subset_dict = {'train':train_names, 'val':val_names, 'test':test_names}
+
+        # Add images
+        for idx, rgb_name in enumerate(subset_dict[subset]):
+            rgb_path = os.path.join(self.image_dir, rgb_name)
+            img_id = idx+self.id_base
+            w, h = self.loadWH(rgb_path)
+            self.add_image(
+                self.name, image_id=img_id,
+                path=rgb_path,
+                width=w,
+                height=h,
+                annotations=self.loadAnn(rgb_name))
+
+    def loadAnn(self, rgb_name): # TODO: is this one correct?
+        '''
+        This function load and return the affordance mask of an rgb image
+        :param rgb_name:
+        :return:
+        '''
+        assert type(rgb_name)==str
+        rgb2txt = lambda f: os.path.splitext(f)[0] + '.txt' # turn extension from jpg to txt
+        aff_path = os.path.join(self.aff_dir, rgb2txt(rgb_name))
+        return np.loadtxt(aff_path, dtype=np.int32)
+
+    def loadWH(self, rgb_path): # TODO: may become the bottleneck
+        '''
+        Return the width and height of an rgb image according to its absolute path.
+        :param rgb_path:
+        :return:
+        '''
+        im = Image.open(rgb_path)
+        return im.size  # (width,height) tuple
+
+    def annToMask(self, ann):
+        '''
+        Turn [height, width] annotations of affordance into [height, width, instances] binary mask.
+        :param ann: return value from self.loadAnns
+        :return:
+        '''
+        # filter all non zero number from matrix and put into a set
+        ann_uni = np.unique(ann)
+        # make binary mask for every number and stack them
+        masks = []
+        class_ids = []
+        for n in ann_uni:
+            if n:
+                class_ids.append(n)
+                masks.append(ann==n)
+        return np.stack(masks, axis=2), class_ids
+
+
+    def load_mask(self, image_id):
+        """Load instance masks for the given image.
+
+        Different datasets use different ways to store masks. This
+        function converts the different mask format to one format
+        in the form of a bitmap [height, width, instances].
+
+        Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance, BG not included.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # separate annotation into [height, width, instance count].
+        assert type(image_id)==int, 'The parameter image_id my be int type.'
+        rgb_name = self.id2name[image_id]
+        return self.annToMask(self.loadAnn(rgb_name))
+
---
+++ train_aff.py
@@ -0,0 +1,192 @@
+"""
+Mask R-CNN
+Configurations and data loading code for MS COCO.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 coco.py train --dataset=/path/to/coco/ --model=coco
+
+    # Train a new model starting from ImageNet weights
+    python3 coco.py train --dataset=/path/to/coco/ --model=imagenet
+
+    # Continue training a model that you had trained earlier
+    python3 coco.py train --dataset=/path/to/coco/ --model=/path/to/weights.h5
+
+    # Continue training the last model you trained
+    python3 coco.py train --dataset=/path/to/coco/ --model=last
+
+    # Run COCO evaluatoin on the last model you trained
+    python3 coco.py evaluate --dataset=/path/to/coco/ --model=last
+"""
+
+import os
+import time
+import numpy as np
+
+# Download and install the Python COCO tools from https://github.com/waleedka/coco
+# That's a fork from the original https://github.com/pdollar/coco with a bug
+# fix for Python 3.
+# I submitted a pull request https://github.com/cocodataset/cocoapi/pull/50
+# If the PR is merged then use the original repo.
+# Note: Edit PythonAPI/Makefile and replace "python" with "python3".
+# from pycocotools.coco import COCO
+# from pycocotools.cocoeval import COCOeval
+# from pycocotools import mask as maskUtils
+
+import zipfile
+import urllib.request
+import shutil
+
+from config import Config
+import utils
+import model as modellib
+
+from iitaff import *
+
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+
+############################################################
+#  Training
+############################################################
+
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN on IIT-AFF.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'evaluate'")
+    parser.add_argument('--dataset', required=True,
+                        metavar="/path/to/iitaff/",
+                        help='Directory of the iit-aff dataset')
+    # parser.add_argument('--year', required=False,
+    #                     default=DEFAULT_DATASET_YEAR,
+    #                     metavar="<year>",
+    #                     help='Year of the MS-COCO dataset (2014 or 2017) (default=2014)')
+    parser.add_argument('--model', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'/'imagenet'/'last'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--limit', required=False,
+                        default=500,
+                        metavar="<image count>",
+                        help='Images to use for evaluation (default=500)')
+    # parser.add_argument('--download', required=False,
+    #                     default=False,
+    #                     metavar="<True|False>",
+    #                     help='Automatically download and unzip MS-COCO files (default=False)',
+    #                     type=bool)
+    args = parser.parse_args()
+    print("Command: ", args.command)
+    print("Model: ", args.model)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = IITAFFConfig()
+    else:
+        class InferenceConfig(IITAFFConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            DETECTION_MIN_CONFIDENCE = 0
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.model.lower() == "coco":
+        model_path = COCO_MODEL_PATH
+    elif args.model.lower() == "last":
+        # Find last trained weights
+        model_path = model.find_last()[1]
+    elif args.model.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        model_path = model.get_imagenet_weights()
+    else:
+        model_path = args.model
+
+    # Load weights
+    print("Loading weights ", model_path)
+    model.load_weights(model_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        # Training dataset. Use the training set and 35K from the
+        # validation set, as as in the Mask RCNN paper.
+        dataset_train = IITAFFDataset()
+        dataset_train.load_iitaff(args.dataset, "train")
+        dataset_train.prepare()
+
+        # Validation dataset
+        dataset_val = IITAFFDataset()
+        dataset_val.load_iitaff(args.dataset, "val")
+        dataset_val.prepare()
+
+        # Training - Stage 1
+        print("Training network heads")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=40,
+                    layers='heads')
+
+        # Training - Stage 2
+        # Finetune layers from ResNet stage 4 and up
+        print("Fine tune Resnet stage 4 and up")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=120,
+                    layers='4+')
+
+        # Training - Stage 3
+        # Fine tune all layers
+        print("Fine tune all layers")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE / 10,
+                    epochs=160,
+                    layers='all')
+
+    elif args.command == "evaluate":
+        # Validation dataset
+        print("Evaluation not implemented yet.")
+        # dataset_val = IITAFFDataset()
+        # iitaff = dataset_val.load_iitaff(args.dataset, "test")
+        # dataset_val.prepare()
+        # print("Running COCO evaluation on {} images.".format(args.limit))
+        # evaluate_coco(model, dataset_val, iitaff, "bbox", limit=int(args.limit))
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'evaluate'".format(args.command))
